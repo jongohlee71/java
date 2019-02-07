8b23adf828f72e649cc9f939484ab405be162864
JONGOH <JONGOH@DESKTOP-AL9KOUG>
2019-02-07 20:31:37 +0900

    ERP 테스트

---
+++ Erp/src/main/java/com/lk/erp/ErpApp.java
@@ -0,0 +1,13 @@
+package com.lk.erp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ErpApp {
+	
+	public static void main(String[] args) {
+		SpringApplication.run(ErpApp.class, args);
+	}
+
+}
---
+++ Erp/src/main/java/com/lk/erp/model/LombokTest.java
@@ -0,0 +1,11 @@
+package com.lk.erp.model;
+
+import lombok.Data;
+
+@Data
+public class LombokTest {
+	
+	private String name;
+	private int price;
+
+}
